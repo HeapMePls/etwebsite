6b5a253e27c22be19b71fb55126d59516122c3b3
HeapMePls <JAFERNANDEZ@correo.um.edu.uy>
2024-09-13 10:49:08 +0900

    ss

--- server.js
+++ server.js
@@ -48,29 +48,25 @@ app.post('/api/send-email', async (req, res) => {
     const { name, email, phone, userAnswers } = req.body;
 
     try {
-        // Create a transporter object
         let transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
-                user: process.env.EMAIL_USER, // Your email address
-                pass: process.env.EMAIL_PASS, // Your email password
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
             },
         });
 
-        // Format the quiz answers into a readable format
         const formattedAnswers = userAnswers.map((answer, index) => {
             return `Question ${index + 1}: ${answer.selectedAnswer} (Type: ${answer.selectedType})`;
         }).join('\n');
 
-        // Set up email data
         let mailOptions = {
-            from: 'ionicfrost143@gmail.com', // Sender address
-            to: 'ionicfrost143@gmail.com',             // Recipient address
+            from: 'info.espaciotres@gmail.com', // Sender address
+            to: 'info.espaciotres@gmail.com',             // Recipient address
             subject: 'Nuevo usuario terminó cuestionario',                // Subject line
             text: `Un usuario contestó el cuestionario!\n\nNombre: ${name}\nE-mail: ${email}\nTeléfono: ${phone}\n\nRespuestas:\n${formattedAnswers}`
         };
 
-        // Send the email
         let info = await transporter.sendMail(mailOptions);
 
         console.log('Email sent: ' + info.response);
