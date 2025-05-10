75e91bde0d90efc48185101f10cd2e930229066a
HeapMePls <JAFERNANDEZ@correo.um.edu.uy>
2025-05-10 09:51:31 +0900

    Add animated leaf background and update styles for about page

--- public/global/global.js
+++ public/global/global.js
@@ -8,3 +8,23 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => document.getElementById("footer").innerHTML = data);
     
 });
+
+const leafContainer = document.querySelector('.leaf-container');
+        const totalLeaves = 30;
+        const leafImages = [
+          '/home/assets/leaves1.png',
+          '/home/assets/leaves2.png'
+        ];
+    
+        for (let i = 0; i < totalLeaves; i++) {
+          const leaf = document.createElement('img');
+          leaf.src = leafImages[Math.floor(Math.random() * leafImages.length)];
+          leaf.classList.add('leaf');
+    
+          leaf.style.top = `${Math.random() * 100}%`;
+          leaf.style.left = `${Math.random() * 100}%`;
+          leaf.style.animationDelay = `${Math.random() * 20}s`;
+          leaf.style.transform = `scale(${Math.random() * 0.5 + 0.5})`;
+    
+          leafContainer.appendChild(leaf);
+        }
